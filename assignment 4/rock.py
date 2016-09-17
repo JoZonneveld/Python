dbb22614911d4277fa7dc57734f7e37ab64b077a
JoZonneveld <joostz14@gmail.com>
2016-09-17 11:16:29 +0200

    tes

--- assignment 4/rock.py
+++ assignment 4/rock.py
@@ -8,48 +8,64 @@ def Rock():
     randompick = ['rock', 'paper', 'scissors']
     computer = (random.choice(randompick))
 
+    global computerwins
+    global playerwins
+
     if computer == "rock":
         var.set("Computer selected " + computer)
-        var2.set("Draw!")
+        var2.set("You draw!")
 
     elif computer == "scissors":
         var.set("Computer selected " + computer)
-        var2.set("Win!")
+        var2.set("You win!")
+        playerwins += 1
 
     elif computer == "paper":
         var.set("Computer selected " + computer)
-        var2.set("Lose!")
+        var2.set("You lose!")
+        computerwins += 1
 
 def Paper():
     randompick = ['rock', 'paper', 'scissors']
     computer = (random.choice(randompick))
 
+    global computerwins
+    global playerwins
+
     if computer == "paper":
         var.set("Computer selected " + computer)
-        var2.set("Draw!")
+        var2.set("You draw!")
 
     elif computer == "rock":
         var.set("Computer selected " + computer)
-        var2.set("Win!")
+        var2.set("You win!")
+        playerwins += 1
 
     elif computer == "scissors":
         var.set("Computer selected " + computer)
-        var2.set("Lose!")
+        var2.set("You lose!")
+        computerwins += 1
 
-def Siccors():
+def scissors():
     randompick = ['rock', 'paper', 'scissors']
     computer = (random.choice(randompick))
+
+    global computerwins
+    global playerwins
+
     if computer == "scissors":
         var.set("Computer selected " + computer)
-        var2.set("Draw!")
+        var2.set("You draw!")
 
     elif computer == "paper":
         var.set("Computer selected " + computer)
-        var2.set("Win!")
+        var2.set("You win!")
+        playerwins += 1
 
     elif computer == "rock":
         var.set("Computer selected " + computer)
-        var2.set("Lose!")
+        var2.set("You lose!")
+        computerwins += 1
 
 root = Tk()
 var = StringVar()
@@ -57,16 +73,16 @@ var2 = StringVar()
 label = Label(root, textvariable=var, relief=FLAT)
 label2 = Label(root, textvariable=var2, relief=FLAT)
 
+var.set("Welcome to rock, paper scissors")
+
 root.title("rock, paper sissors")
-root.geometry("150x125")
+root.geometry("175x125")
 
 app = Frame(root)
 app.grid()
 button1 = Button(app, text = "Rock", command = Rock)
 button2 = Button(app, text = "Paper", command = Paper)
-button3 = Button(app, text = "Siccors", command = Siccors)
-
-
+button3 = Button(app, text = "scissors", command = scissors)
 
 button1.grid()
 button2.grid()
