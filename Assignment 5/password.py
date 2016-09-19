3a587a533b5fd012bd7fead033e51c7ea88b3237
JoZonneveld <joostz14@gmail.com>
2016-09-19 08:57:14 +0200

    next

--- Assignment 5/password.py
+++ Assignment 5/password.py
@@ -1,13 +1,17 @@
+import re
 password = input("Vul uw wachtwoord in: \n")
 
 i = len(password)
 
-if i > 15:
-    print("Strong")
-elif i > 7 and i < 15:
-    print("Medium")
-elif i < 7:
-    print("Weak")
+if re.match("^[a-z0-9_]*$", password):
+    uppercase = 0
+    special = 0
+elif re.match("^[A-Z_]*$", password):
+    uppercase = 1
+else:
+    special = 1
 
-#joost
-
+if i < 8 and uppercase == 1 or special == 1:
+    print("kleiner dan 8 en uppercase is used")
+elif i < 8 and special == 1:
+    print("kleiner dan 8 en special is used")
