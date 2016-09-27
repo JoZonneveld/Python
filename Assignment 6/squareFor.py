dae3a2153231f45dbbb6ac7c5d98f839977a58a7
JoZonneveld <joostz14@gmail.com>
2016-09-27 12:09:51 +0200

    .

--- Assignment 6/squareFor.py
+++ Assignment 6/squareFor.py
@@ -1,6 +1,6 @@
-getal = int(input("Voer een getal in: \n"))         # (1, position) -> (PC + 1, S[position-> S.position + (3.0)])
-for a in range(getal):                              #
-    output = ""                                     #
-    for i in range(getal):                          #
-        output = output + "*"                       #
-    print(output)                                   #
+getal = int(input("Voer een getal in: \n"))         # (1, S.getal) -> (PC + 1, S[getal-> int(input(S.getal + (5)))])
+for a in range(getal):                              # (2,S) ->fVRB(firstLine(B, S) when S[a] == range(getal) | (PC,S) ->fVRB(skipAfter(B, S) when S[a] != range getal)
+    output = ""                                     # (3, S.output) -> (PC + 1, S[output-> S.output + ()])
+    for i in range(getal):                          # (4,S) ->fVRB(firstLine(B, S) when S[a] == range(getal) | (PC,S) ->fVRB(skipAfter(B, S) when S[a] != range getal)
+        output = output + "*"                       # (5, S.output) -> (PC + 1, S[output-> S.output + S.output + (*)])
+    print(output)                                   # (3, S.output) -> (PC + 1, S[output-> print(S.output)])
