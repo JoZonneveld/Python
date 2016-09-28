87e10797b7ea560c9a140b78bc1d646e43180ac1
JoZonneveld <joostz14@gmail.com>
2016-09-28 13:48:02 +0200

    .

--- Assignment 6/squareFor.py
+++ Assignment 6/squareFor.py
@@ -3,4 +3,4 @@ for a in range(getal):                              # (2,S) ->fVRB(firstLine(B, 
     output = ""                                     # (3, S.output) -> (PC + 1, S[output-> S.output + ()])
     for i in range(getal):                          # (4,S) ->fVRB(firstLine(B, S) when S[a] == range(getal) | (PC,S) ->fVRB(skipAfter(B, S) when S[a] != range getal)
         output = output + "*"                       # (5, S.output) -> (PC + 1, S[output-> S.output + S.output + (*)])
-    print(output)                                   # (3, S.output) -> (PC + 1, S[output-> print(S.output)])
+    print(output)                                   # (6, S.output) -> (PC + 1, S[output-> print(S.output)])
