783b50a2a382868f34e5aa9166cb82e8e9331a7d
JoZonneveld <joostz14@gmail.com>
2016-11-21 17:26:15 +0100

    .

--- periode 1/Assignment 6/test.py
+++ periode 1/Assignment 6/test.py
@@ -1,4 +1,9 @@
-count  = 1
-for i in range(1,5):
-    count *= i
-    print(count, " - ", i)
+output = ""
+for i in range(0, 4):
+    for j in range(0, 4):
+        if (i + j) % 2 == 0:
+            output += "0"
+        else:
+            output += "="
+    output += "\n"
+print(output)
