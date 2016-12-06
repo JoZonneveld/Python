ef9c97b827f1480f0d28e8ef4fc1d21640b9988f
JoZonneveld <joostz14@gmail.com>
2016-12-06 21:15:52 +0100

    .

---
+++ periode 2/les4/Assignment3.3.py
@@ -0,0 +1,17 @@
+def repeat (symbol, n):
+    res=""
+    while((n>0)):
+        res = (res + symbol)
+        n = (n-1)
+    return res
+
+def draw_line(n):
+    return repeat("*",n)
+
+def draw_square(n):
+    l = (draw_line(n) + "\n")
+    return repeat(l, n)
+
+res = draw_square(5)
+
+print(res)
