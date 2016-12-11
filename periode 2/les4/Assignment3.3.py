1b1753b778156e7786ffe7e1c2b6a55ad5a9b2c9
JoZonneveld <joostz14@gmail.com>
2016-12-11 14:05:30 +0100

    .

--- periode 2/les4/Assignment3.3.py
+++ periode 2/les4/Assignment3.3.py
@@ -12,6 +12,12 @@ def draw_square(n):
     l = (draw_line(n) + "\n")
     return repeat(l, n)
 
-res = draw_square(5)
+def draw_empty_square(n):
+    l = (draw_line(n) + "\n")
+    empty_line=("*" + (repeat(" ", (n-2)) + "*\n"))
+    return (l + empty_line + l)
+
+
+res = draw_empty_square(3)
 
 print(res)
