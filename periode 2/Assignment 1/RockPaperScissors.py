768d3e030cbc31d4f985b758a93abfe79b99495d
JoZonneveld <joostz14@gmail.com>
2016-12-18 23:27:47 +0100

    .
    Nieuwe indeling en assignments goed gezet

--- periode 2/Assignment 1/RockPaperScissors.py
+++ periode 2/Assignment 1/RockPaperScissors.py
@@ -17,12 +17,8 @@ class RockPaperScissors:
         play1 = Player(self.player1, 0)
         play2 = Player(self.player2, 0)
 
-<<<<<<< HEAD
-        while scorep2 !=2 and scorep1 !=2:
-=======
         while play2.score !=2 and play1.score !=2:
 
->>>>>>> origin/master
             randompick = [0, 1, 2]
             #0 = rock
             #1 = paper
@@ -51,21 +47,24 @@ class RockPaperScissors:
                 print("Player 1 pakt scissors, Player 2 pakt paper. P1 wint")
                 play1.increaseScore(1)
 
-<<<<<<< HEAD
             scorep1 = play1.score
             scorep2 = play2.score
             if scorep2 !=2 and scorep1 !=2:
                 print("Stand P1:", scorep1, " - P2:", scorep2)
         print("Eindstand P1:", scorep1, " - P2:", scorep2)
         if scorep1 > scorep2:
-=======
+
             if play1.score != 2 and play2.score != 2:
                 print("Stand P1:", play1.score, " - P2:", play2.score)
 
 
         print("Eindstand P1:", play1.score, " - P2:", play2.score)
         if play1.score > play2.score:
->>>>>>> origin/master
+
             print("P1 Wint!!!")
         else:
             print("P2 Wint!!!")
+
+game = RockPaperScissors("test", "test1")
+
+game.pick()
